8586a1180dbb18f4772e2b4df1d650a0d8a82383
Ahmad <ahmadbunni@gmail.com>
2023-09-27 19:18:20 +0300

    ز

--- resources/views/website/authors/index.blade.php
+++ resources/views/website/authors/index.blade.php
@@ -5,12 +5,10 @@
             <div class="pb-lg-1">
                 <div class="page-header border-bottom">
                     <div class="container">
-                        <div class="d-md-flex justify-content-between align-items-center py-4">
-                            <h1 class="page-title font-size-3 font-weight-medium m-0 text-lh-lg">المؤلفون</h1>
+                        <div class="d-md-flex justify-content-between align-items-center py-4 rtl-container">
                             <nav class="woocommerce-breadcrumb font-size-2">
-                                <a href="../home/index.html" class="h-primary">الصفحة الرئيسية</a>
-                                <span class="breadcrumb-separator mx-1"><i class="fas fa-angle-left"></i></span>
-                                <span>المؤلفون</span>
+                                <a href="https://daralsamman.com" class="h-primary">الصفحة الرئيسية</a>
+                                <span class="breadcrumb-separator mx-1"><i class="fas fa-angle-left"></i></span>المؤلفون
                             </nav>
                         </div>
                     </div>

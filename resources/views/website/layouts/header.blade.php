2c120cdba9fe0462926e8ed77f01a5e4b6d6884a
abdallahsasa <abdallah97sasa@gmail.com>
2023-10-31 12:45:27 +0300

    mobile menu

--- resources/views/website/layouts/header.blade.php
+++ resources/views/website/layouts/header.blade.php
@@ -28,6 +28,30 @@
                         </div>
                     </form>
                 </div>
+                <div class="offcanvas-toggler mr-5 hmenu">
+                    <a id="sidebarNavToggler2" href="javascript:;" role="button" class="cat-menu"
+                       aria-controls="sidebarContent2"
+                       aria-haspopup="true"
+                       aria-expanded="false"
+                       data-unfold-event="click"
+                       data-unfold-hide-on-scroll="false"
+                       data-unfold-target="#sidebarContent2"
+                       data-unfold-type="css-animation"
+                       data-unfold-overlay='{
+                            "className": "u-sidebar-bg-overlay",
+                            "background": "rgba(0, 0, 0, .7)",
+                            "animationSpeed": 100
+                        }'
+                       data-unfold-animation-in="fadeInLeft"
+                       data-unfold-animation-out="fadeOutLeft"
+                       data-unfold-duration="100">
+                        <svg width="20px" height="18px">
+                            <path fill-rule="evenodd"  fill="rgb(25, 17, 11)" d="M-0.000,-0.000 L20.000,-0.000 L20.000,2.000 L-0.000,2.000 L-0.000,-0.000 Z"/>
+                            <path fill-rule="evenodd"  fill="rgb(25, 17, 11)" d="M-0.000,8.000 L15.000,8.000 L15.000,10.000 L-0.000,10.000 L-0.000,8.000 Z"/>
+                            <path fill-rule="evenodd"  fill="rgb(25, 17, 11)" d="M-0.000,16.000 L20.000,16.000 L20.000,18.000 L-0.000,18.000 L-0.000,16.000 Z"/>
+                        </svg>
+                    </a>
+                </div>
 
             </div>
         </div>
@@ -37,7 +61,6 @@
                     <div class="site-navigation mr-auto d-none d-xl-block">
                         <ul class="nav pl-xl-4">
                             <li class="nav-item"><a href="{{route('website.home')}}" class="nav-link link-black-100 mx-3 px-0 py-3 font-size-2 font-weight-medium">الصفحة الرئيسية</a></li>
-{{--                            Start NEW--}}
                             <li class="nav-item dropdown">
                                 <a id="shopDropdownInvoker"
                                    href="{{route('website.products.index')}}"
@@ -89,7 +112,6 @@
 
                                 </ul>
                             </li>
-
                             <li class="nav-item"><a href="{{route('website.suppliers.index')}}" class="nav-link link-black-100 mx-3 px-0 py-3 font-size-2 font-weight-medium">الموزعون المعتمدون</a></li>
                             <li class="nav-item dropdown">
                                 <a id="homeDropdownInvoker" href="javascript:void(0)"
@@ -122,9 +144,83 @@
                             <li class="nav-item"><a href="{{route('website.contact')}}" class="nav-link link-black-100 mx-3 px-0 py-3 font-size-2 font-weight-medium">تواصل معنا</a></li>
                         </ul>
                     </div>
-
                 </div>
             </div>
         </div>
+        <!-- Categories Sidebar Navigation -->
+        <aside id="sidebarContent2" class="u-sidebar u-sidebar__md u-sidebar--left" aria-labelledby="sidebarNavToggler2">
+            <div class="u-sidebar__scroller js-scrollbar">
+                <div class="u-sidebar__container">
+                    <div class="u-header-sidebar__footer-offset">
+                        <!-- Content -->
+                        <div class="u-sidebar__body">
+                            <div class="u-sidebar__content u-header-sidebar__content">
+                                <!-- Title -->
+                                <header class="border-bottom px-4 px-md-5 py-4 d-flex align-items-center justify-content-between">
+                                    <h2 class="font-size-3 mb-0">Daralsamman</h2>
+
+                                    <!-- Toggle Button -->
+                                    <div class="d-flex align-items-center">
+                                        <button type="button" class="close ml-auto"
+                                                aria-controls="sidebarContent2"
+                                                aria-haspopup="true"
+                                                aria-expanded="false"
+                                                data-unfold-event="click"
+                                                data-unfold-hide-on-scroll="false"
+                                                data-unfold-target="#sidebarContent2"
+                                                data-unfold-type="css-animation"
+                                                data-unfold-animation-in="fadeInLeft"
+                                                data-unfold-animation-out="fadeOutLeft"
+                                                data-unfold-duration="500">
+                                            <span aria-hidden="true"><i class="fas fa-times ml-2"></i></span>
+                                        </button>
+                                    </div>
+                                    <!-- End Toggle Button -->
+                                </header>
+                                <!-- End Title -->
+                                <div class="border-bottom">
+                                    <div class="zeynep pt-4">
+                                        <ul>
+                                            <li class="nav-item"><a href="{{route('website.home')}}" class="nav-link link-black-100 mx-3 px-0 py-3 font-size-2 font-weight-medium">الصفحة الرئيسية</a></li>
+                                            <li class="has-submenu">
+                                                <a href="#" data-submenu="travel">                                    إصدارات الدار
+                                                </a>
+                                                <div id="travel" class="submenu">
+                                                    <div class="submenu-header" data-submenu-close="travel">
+                                                        <a href="#">                                    إصدارات الدار
+                                                        </a>
+                                                    </div>
+
+                                                    <ul>
+                                                        <li>
+                                                            <a href="{{route('website.products.index')}}">الكل</a>
+                                                        </li>
+                                                        @foreach($MenuCategories as $category )
+                                                                    <li>
+                                                                        <a href="{{route('website.products.category.index',$category->id)}}">{{$category->name}}</a>
+                                                                    </li>
+                                                        @endforeach
+                                                    </ul>
+                                                </div>
+                                            </li>
+                                            <li class="nav-item"><a href="{{route('website.suppliers.index')}}" class="nav-link link-black-100 mx-3 px-0 py-3 font-size-2 font-weight-medium">الموزعون المعتمدون</a></li>
+                                            <li class="nav-item"><a href="{{route('website.scientific.index')}}" class="nav-link link-black-100 mx-3 px-0 py-3 font-size-2 font-weight-medium">اللجنة العلمية</a></li>
+                                            <li class="nav-item"><a href="{{route('website.authors.index')}}" class="nav-link link-black-100 mx-3 px-0 py-3 font-size-2 font-weight-medium" >المؤلفون</a></li>
+                                            <li class="nav-item"><a href="{{route('website.auditors.index')}}" class="nav-link link-black-100 mx-3 px-0 py-3 font-size-2 font-weight-medium">المحققون</a></li>
+                                            <li class="nav-item"><a href="javascript:void(0)" class="nav-link link-black-100 mx-3 px-0 py-3 font-size-2 font-weight-medium">مقالات</a></li>
+                                            <li class="nav-item"><a href="{{route('website.about')}}" class="nav-link link-black-100 mx-3 px-0 py-3 font-size-2 font-weight-medium">تأسيس الدار</a></li>
+                                            <li class="nav-item"><a href="{{route('website.contact')}}" class="nav-link link-black-100 mx-3 px-0 py-3 font-size-2 font-weight-medium">تواصل معنا</a></li>
+                                        </ul>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                        <!-- End Content -->
+                    </div>
+                </div>
+            </div>
+        </aside>
+        <!-- End Categories Sidebar Navigation -->
     </div>
 </header>
